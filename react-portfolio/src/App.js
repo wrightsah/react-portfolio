511e3ba555e8af836d46238331068e53e139bd0c
Stephen Wright <wrightsah@icloud.com>
2021-05-18 00:44:47 -0400

    created basic Home - About Me section

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from './components/Footer';
 import { BrowserRouter as Router } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
+        <Home />
         <Footer />
 
       </Router>
---
+++ react-portfolio/src/pages/Home.js
@@ -0,0 +1,35 @@
+// imports
+import React from "react";
+
+// function
+// content from previous portfolio
+
+const Home = () => {
+  return (
+    <div>
+      <div class="container">
+
+<div class="col-md-10">
+  <div class="row">
+    <h1 class="col-md-10">About Me</h1>
+    <hr />
+  </div>
+  <div class="row">
+    <div>
+      <img class="float-left" src="assets/952C0ACE-34D3-4FEC-9677-5BD6092662B0_1_105_c.jpeg" alt="Profile Picture" />
+      <p>I am a hospitality professional and developing developer with interests in sustainability and ethical leadership. Nearly a decade of experience in multiple levels of hotel and operational leadership have shaped my perspective and insight about what an online solution requires to be successful. I do not believe that a one-size-fits-all approach to application design is effective, and that the best results come from truly understanding an organization's goal and the people who endeavor to achieve it.</p>
+      <p>My objective is to work with organizations, such as hotels and non-profits, to develop web-based applications that optimize interactions among all levels of their team, while accounting for common challenges, such as language barriers or differing abilities.</p>
+    </div>
+  </div>
+</div>
+
+</div>
+
+    </div>
+  ) 
+};
+
+// export 
+
+export default Home; 
+
