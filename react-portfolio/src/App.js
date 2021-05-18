52cc1c7d5ab4fd662cd3795183394f0e42580fb1
Stephen Wright <wrightsah@icloud.com>
2021-05-18 00:23:09 -0400

    created basic footer

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
+import Footer from './components/Footer';
 import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
@@ -7,6 +8,8 @@ function App() {
     <div className="App">
       <Router>
         <Header />
+        <Footer />
+
       </Router>
     </div>
   );
---
+++ react-portfolio/src/components/Footer.js
@@ -0,0 +1,18 @@
+// import react 
+
+import React from 'react';
+
+// function 
+
+function Footer() {
+    return(
+        <div>
+            This is a footer
+        </div>
+    )
+}
+
+
+// export 
+
+export default Footer; 
